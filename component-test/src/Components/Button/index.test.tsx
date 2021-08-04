7eb23bd265328c9600e3fc4aaf77268a8ee5c3dd
amuse1991 <amuse1991@gmail.com>
2021-08-04 14:33:05 +0900

    Button 컴포넌트 onClick 테스트 추가

--- component-test/src/Components/Button/index.test.tsx
+++ component-test/src/Components/Button/index.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import "jest-styled-components";
 
 import { Button } from "./index";
@@ -16,5 +16,35 @@ describe("<Button/>", () => {
     });
 
     expect(container).toMatchSnapshot();
+  });
+
+  it("change backgroundColor and hoverColor Props", () => {
+    const backgroundColor = "#FF1744";
+    const hoverColor = "#F01440";
+    render(
+      <Button
+        label="ButtonTest"
+        backgroundColor={backgroundColor}
+        hoverColor={hoverColor}
+      />
+    );
+
+    const parent = screen.getByText("ButtonTest").parentElement;
+    expect(parent).toHaveStyleRule("background-color", backgroundColor);
+    expect(parent).toHaveStyleRule("background-color", hoverColor, {
+      modifier: ":hover",
+    });
+  });
+
+  it("click the button", () => {
+    const handleClick = jest.fn();
+    render(<Button label="ButtonTest" onClick={handleClick} />);
+
+    const label = screen.getByText("ButtonTest");
+    // Button 컴포넌트가 한번도 클릭되지 않았음
+    expect(handleClick).toHaveBeenCalledTimes(0);
+    fireEvent.click(label); // 클릭 이벤트 fire
+    // Button 컴포넌트의 onClick 메소드가 1회 실행되었음
+    expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
